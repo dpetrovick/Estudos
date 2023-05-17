582c4384c3a2fd04d7464aa8f23f817b00445045
DenysPetrovick <dpetrovick@hotmail.com>
2023-05-17 16:11:40 -0300

    ex aulas 11 e 12

---
+++ Java Script/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`Sua velocidade computada foi de ${vel}km/h!`)
+if (vel > 80) {
+    console.log(`A velicade ${vel}km/h está acima do permitodo que é de 80km/h. Você foi multado!!`)
+}
+console.log("Dirija com segurança e prudência!")
---
+++ Java Script/Aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "Brasil"
+console.log(`Nascido em país ${pais}`)
+if (pais == "Brasil") {
+    console.log("Você é Brasileiro!")
+}
+else {
+    console.log("Você é estrangeiro!")
+}
